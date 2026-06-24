f827ef6af09c6244827588322426b91350fa4ab4
Ricardo <ricardo.lage.tech@gmail.com>
2026-06-24 10:06:25 -0300

    Adicionando fixtures para gerir os corpos das requisições

--- test/Authorization.test.js
+++ test/Authorization.test.js
@@ -1,19 +1,18 @@
 const request = require('supertest');
 const { expect } = require('chai');
 require('dotenv').config();
+const postAuthLogin = require('../fixtures/postAuthLogin.json');
 
 describe('Authorization', () => {
     describe('POST /auth/login', () => {
         it('Deve retornar 200 com token em string quando as credenciais são válidas', async () => {
-            // Simule uma requisição POST para /login com credenciais válidas
-            // com o supertest e capture a resposta
+            const bodyAuthlogin = { ...postAuthLogin}; // Usando o fixture postAuthLogin.json para enviar o corpo da requisição em shallow copy. Se o json tivesse mais subniveis, seria necessário usar deep copy, mas como o json é simples, shallow copy é suficiente.
+            
+            // Simule uma requisição POST para /login com credenciais válidas com o supertest e capture a resposta
             const resposta = await request(process.env.BASE_URL)
                 .post('/auth/login')
                 .set('Content-Type', 'application/json')
-                .send({
-                    email: "ricardo@email.com",
-                    senha: "12345678"
-                })
+                .send(bodyAuthlogin)
 
             //console.log(resposta.body); //somente para verificar o corpo da resposta...deve ser apagado
             //console.log(resposta.status); //somente para verificar o status da resposta...deve ser apagado
@@ -23,5 +22,20 @@ describe('Authorization', () => {
             expect(resposta.status).to.equal(200);
             expect(resposta.body).to.have.property('token').that.is.a('string');
         })
+
+        it('Deve retornar 401 quando as credenciais são inválidas', async () => {
+            const bodyAuthlogin = { ...postAuthLogin};
+            bodyAuthlogin.email = "email@invalido.com";
+            bodyAuthlogin.senha = "senha_invalida";
+
+            const resposta = await request(process.env.BASE_URL)
+                .post('/auth/login')
+                .set('Content-Type', 'application/json')
+                .send(bodyAuthlogin)
+
+            expect(resposta.status).to.equal(401);
+            expect(resposta.body).to.have.property('erro').that.is.a('string').to.equal('Credenciais inválidas');
+        })
+
     })
 })
--- test/Cdi.test.js
+++ test/Cdi.test.js
@@ -2,6 +2,7 @@ const request = require('supertest');
 const { expect } = require('chai');
 require('dotenv').config();
 const { obterToken } = require('../helpers/autenticacao');
+const postCdi = require('../fixtures/postCdi.json');
 
 describe('CDI', () => {
     describe('POST /cdi', () => {
@@ -10,22 +11,23 @@ describe('CDI', () => {
         
         //before é um hook do Mocha que é executado antes de todos os testes dentro do bloco describe.
         //beforeEach é um hook do Mocha que é executado antes de cada teste dentro do bloco describe.
-
         beforeEach (async () => {
             // Antes de executar os testes, obtenha o token de autenticação
             token = await obterToken("ricardo@email.com", "12345678");
         });
-        it('Deve retornar sucesso com 201 quando o valor configurado do CDI maior que zero ', async () => {
 
+
+        it('Deve retornar sucesso com 201 quando o valor configurado do CDI maior que zero ', async () => {
             //const token = await obterToken("ricardo@email.com", "12345678"); //chamando a função obterToken do arquivo autenticacao.js para capturar o token de autenticação
+
+            const bodyCdi = { ...postCdi}; // Usando o fixture postCdi.json para enviar o corpo da requisição em shallow copy. Se o json tivesse mais subniveis, seria necessário usar deep copy, mas como o json é simples, shallow copy é suficiente.
 
             const resposta = await request(process.env.BASE_URL)
                 .post('/cdi')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', `Bearer ${token}`)
-                .send({
-                    valor: 10.75
-                })
+                .send(bodyCdi)
+
         expect(resposta.status).to.equal(201)
         //expect(resposta.body).to.have.property('message', 'CDI configurado com sucesso')
         
@@ -33,16 +35,17 @@ describe('CDI', () => {
         })
 
         it('Deve retornar falha com 400 quando o valor configurado do CDI for menor ou igual a zero', async () => {
-
             //const token = await obterToken("ricardo@email.com", "12345678"); //chamando a função obterToken do arquivo autenticacao.js para capturar o token de autenticação
             
+            const bodyCdi = {...postCdi};
+            bodyCdi.valor = 0; // Alterando o valor para 0, que é um valor inválido
+
             const resposta = await request(process.env.BASE_URL)
                 .post('/cdi')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', `Bearer ${token}`)
-                .send({
-                    valor: 0
-                })
+                .send(bodyCdi)
+
         expect(resposta.status).to.equal(400)
         //expect(resposta.body).to.have.property('error', 'Valor do CDI deve ser maior que zero')
 
